96bc64b085010f6b7451db984d2c3a5db000b890
Harish Swaminathan <harishswami62@gmail.com>
2024-01-04 12:31:08 +0530

    update cart

--- ecobazar-ecom/src/components/homepage/popularCategories/PopularSection.js
+++ ecobazar-ecom/src/components/homepage/popularCategories/PopularSection.js
@@ -2,13 +2,23 @@ import PopularCategoryAll from "./PopularCategoryAll";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight} from '@fortawesome/free-solid-svg-icons';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import { addCategory } from "../../../redux/action";
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
 function PopularSection(){
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+    const handleNavigation =()=>{
+        dispatch(addCategory("allcategory"))
+        navigate("/categories");
+      }
     return(
         <div className="PopularSection d-flex">
             <div className="viewAllAndTitle">
                 <div className="title"><h2>Popular Categories</h2> </div>
-                <div className="ViewAll"><Link to="/categories">View All  <FontAwesomeIcon icon={faArrowRight} /></Link></div>
+                <div className="ViewAll" onClick={handleNavigation}>View All  <FontAwesomeIcon icon={faArrowRight} /></div>
             </div>
             <PopularCategoryAll/>
         </div>  
